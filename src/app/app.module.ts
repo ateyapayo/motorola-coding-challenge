e37246c6a7934d0115ff14cbb2b7e9519307e544
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-11-09 17:33:55 +0000

    feat: minimal styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from "./components/home/home.component";
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
