2190cd95133f59552786daef512c12393bf7ea21
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-11-09 17:57:10 +0000

    feat: basic unit test

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,23 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { LoginComponent } from "./login.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AuthService } from "../../auth.service";
+import { Router } from "@angular/router";
 
-import { LoginComponent } from './login.component';
-
-describe('LoginComponent', () => {
+describe("LoginComponent", () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    authService = jasmine.createSpyObj("AuthService", ["login"]);
+    router = jasmine.createSpyObj("Router", ["navigate"]);
+
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule],
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: Router, useValue: router },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create the LoginComponent", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should submit the form with valid credentials", () => {
+    component.loginForm.get("username")?.setValue("validusername");
+    component.loginForm.get("password")?.setValue("validpassword");
+
+    authService.login.and.returnValue(true);
+
+    component.onSubmit();
+
+    expect(authService.login).toHaveBeenCalledWith(
+      "validusername",
+      "validpassword"
+    );
+    expect(router.navigate).toHaveBeenCalledWith(["/home"]);
+  });
+
+  it("should show error messages for invalid credentials", () => {
+    component.loginForm.get("username")?.setValue("invalidusername");
+    component.loginForm.get("password")?.setValue("");
+
+    component.onSubmit();
+
+    expect(component.passwordError).toBe("Please enter a password.");
+  });
+
+  it("should show a credentials error message for empty fields", () => {
+    component.onSubmit();
+
+    expect(component.credentialsError).toBe("Please enter some credentials.");
+  });
+
+  it("should show a credentials error message for failed authentication", () => {
+    component.loginForm.get("username")?.setValue("validusername");
+    component.loginForm.get("password")?.setValue("validpassword");
+
+    authService.login.and.returnValue(false);
+
+    component.onSubmit();
+
+    expect(authService.login).toHaveBeenCalledWith(
+      "validusername",
+      "validpassword"
+    );
+    expect(component.credentialsError).toBe("Invalid username or password.");
   });
 });
